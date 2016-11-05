d5b8c6e19862ca2be7b4a0e126f79f65c3c650cf
Ronflex <vincent.jeannas@fromthebard.com>
2016-11-05 14:10:29 +0100

    Debut de time

--- VikingRaider/Assets/Scripts/Time.cs
+++ VikingRaider/Assets/Scripts/Time.cs
@@ -4,5 +4,11 @@ using System.Collections;
 public class Time : MonoBehaviour {
 
     public int currentTurn { get; set; }
+    public bool event_occuring { get; set; }
 
+    public void update ()//GameManager gamemanager)
+    {
+        //liste de villes => villes updated
+        //update event
+    }  
 }
