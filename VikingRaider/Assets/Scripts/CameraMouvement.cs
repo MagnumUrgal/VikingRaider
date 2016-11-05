5c1264b3b834a089b408d7df368398c6c9a05ae2
ClementKarrer <magnum.urgal@gmail.com>
2016-11-05 12:58:51 +0100

    [GUI Commit] Map + Base scene GUI

---
+++ VikingRaider/Assets/Scripts/CameraMouvement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMouvement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 cursorPos = this.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+            //transform.Translate(cursorPos - this.GetComponent<Camera>());
+            //transform.LookAt(cursorPos);
+        }
+    }
+}
