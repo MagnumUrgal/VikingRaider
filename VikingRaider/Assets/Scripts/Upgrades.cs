35e350bd645657f1eb892bce2a99785b4d05d2dc
Groot <malus3@orange.fr>
2016-11-05 11:41:10 +0100

    Creation Upgrade

--- VikingRaider/Assets/Scripts/Upgrades.cs
+++ VikingRaider/Assets/Scripts/Upgrades.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class Upgrades : MonoBehaviour
 {
 
+    // potentiellement mettre les autres valeurs, et le GameManager ne fera que changer les valeurs ici
     protected int costViking { get; set; }
-    protected int costMercFai { get; set; }
+    // protected int costMercFai { get; set; }
     protected int costMercMoy { get; set; }
-    protected int costMercFor { get; set; }
-    // potentiellement mettre les autres valeurs, et la GameManager ne fera que changer les valeurs ici
+    // protected int costMercFor { get; set; }
+    protected int costAtkUp { get; set; }
+    protected int costDefUp { get; set; }
+    protected int costMoralUp { get; set; }
+    protected int costIntimidateUp { get; set; }
+    protected int costDecrease { get; set; }
 
     // Je considère qu'on gère dans le GameManager le fait qu'on vérifie si le joueur a assez d'argent pour cliquer sur le bouton
 
@@ -20,21 +25,21 @@ public class Upgrades : MonoBehaviour
             drakkar.viking.add(n);
             drakkar.gold -= costViking;
         }
-        else if (drakkar.merc_faibles.name == _name)
-        {
-            drakkar.merc_faibles.add(n);
-            drakkar.gold -= costMercFai;
-        }
+        //else if (drakkar.merc_faibles.name == _name)
+        //{
+        //    drakkar.merc_faibles.add(n);
+        //    drakkar.gold -= costMercFai;
+        //}
         else if (drakkar.merc_moyens.name == _name)
         {
             drakkar.merc_moyens.add(n);
             drakkar.gold -= costMercMoy;
         }
-        else if (drakkar.merc_forts.name == _name)
-        {
-            drakkar.merc_forts.add(n);
-            drakkar.gold -= costMercFor;
-        }
+        //else if (drakkar.merc_forts.name == _name)
+        //{
+        //    drakkar.merc_forts.add(n);
+        //    drakkar.gold -= costMercFor;
+        //}
     }
 
     public void recrutEspion(Drakkar drakkar, Espion espion, int costEspion)
@@ -49,32 +54,32 @@ public class Upgrades : MonoBehaviour
     // if (drakkar.espion_list.Count != drakkar.espion_list.Capacity)
 
     // peut etre mettre chaque costStat dans la classe
-    public void UpgradeStat(Drakkar drakkar, string _stat, int costStatUpgrade, int upValue)
+    public void UpgradeStat(Drakkar drakkar, string _stat, int upValue)
     {
         // potentiellement refaire le systeme de switch
         if (_stat == "atk")
         {
             drakkar.viking.atk += upValue;
-            drakkar.gold -= costStatUpgrade;
+            drakkar.gold -= costAtkUp;
         }
         else if (_stat == "def")
         {
             drakkar.viking.def += upValue;
-            drakkar.gold -= costStatUpgrade;
+            drakkar.gold -= costDefUp;
         }
         else if (_stat == "moral")
         {
             drakkar.viking.moral += upValue;
-            drakkar.gold -= costStatUpgrade;
+            drakkar.gold -= costMoralUp;
         }
         else if (_stat == "intimidate")
         {
             drakkar.viking.intimidate += upValue;
-            drakkar.gold -= costStatUpgrade;
+            drakkar.gold -= costIntimidateUp;
         }
     }
 
-    public void DecreaseMinMembers(Drakkar drakkar, int costDecrease, int subValue)
+    public void DecreaseMinMembers(Drakkar drakkar, int subValue)
     {
         drakkar.min_members -= subValue;
         drakkar.gold -= costDecrease;
