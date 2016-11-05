2aaddf4fc7d9ac90abcccb0d0eb1925f503ca7ad
Groot <malus3@orange.fr>
2016-11-05 02:26:01 +0100

    Class Villes, Drakkar. Maj Perso

---
+++ VikingRaider/Assets/Scripts/Drakkar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drakkar : MonoBehaviour
+{
+    protected string nameDrakkar { get; set; }
+    protected int or { get; set; }
+    protected Soldat viking { get; set; }
+    protected Soldat merc_faibles { get; set; }
+    protected Soldat merc_moyens { get; set; }
+    protected Soldat merc_forts { get; set; }
+
+    public Drakkar(string _name, int _or, Soldat _viking, Soldat _mfaibles, Soldat _mmoyens, Soldat _mforts)
+    {
+        nameDrakkar = _name;
+        or = _or;
+        viking = _viking;
+        merc_faibles = _mfaibles;
+        merc_moyens = _mmoyens;
+        merc_forts = _mforts;
+    }
+}
---
+++ VikingRaider/Assets/Scripts/Villes.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Villes : MonoBehaviour
+{
+    protected string nameVilles { get; set; }
+    protected int fortification { get; set; }
+    protected int or { get; set; }
+    protected Soldat garnison { get; set; }
+    protected bool king { get; set; }
+    protected int capture { get; set; }
+    protected int perception { get; set; }
+    protected int productivity { get; set; }
+    protected int fear { get; set; }
+
+    public Villes(string _name, int _fortif, int _or, Soldat _unite, int _capture, int _perception, int _productivity, int _fear)
+    {
+        king = false;
+        nameVilles = _name;
+        fortification = _fortif;
+        or = _or;
+        garnison = _unite;
+        capture = _capture;
+        perception = _perception;
+        productivity = _productivity;
+        fear = _fear;
+    }
+}
